b0f51cabbd8e1c999f7aa197039231e7ee06bcd4
URK21CS1067 <93753029+URK21CS1067@users.noreply.github.com>
2022-07-27 15:48:13 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -49,6 +49,8 @@ class Evaluate:
       operand: The operand to be pushed.
     """
     # Write your code here
+    if self.top != self.size_of_stack - 1:
+      self.stack.append(operand)
 
 
   def validate_postfix_expression(self, expression):
@@ -60,6 +62,17 @@ class Evaluate:
       True if the expression is valid, else returns False.
     """
     # Write your code here
+    a = 0
+    b = 0
+    for element in expression:
+      if element.isnumeric():
+        a = a + 1
+      else:
+        b = b + 1
+    if b == a - 1:
+      return True
+    else:
+      return False
 
 
   def evaluate_postfix_expression(self, expression):
@@ -71,6 +84,27 @@ class Evaluate:
       The result of evaluated postfix expression.
     """
     # Write your code here
+    stack = []
+    for i in expression:
+      if i.isnumeric():
+        stack.append(int(i))
+      if len(stack) >= 2:
+        if i == '+':
+          stack[-2] = stack[-2] + stack[-1]
+          stack.pop()
+        elif i == '-':
+          stack[-2] = stack[-2] - stack[-1]
+          stack.pop()
+        elif i == '*':
+          stack[-2] = stack[-2] * stack[-1]
+          stack.pop()
+        elif i == '/':
+          stack[-2] = stack[-2] / stack[-1]
+          stack.pop()
+        elif i == '^':
+          stack[-2] = stack[-2] ^ stack[-1]
+          stack.pop()
+    return int(stack[-1])
 
 
 # Do not change the following code
